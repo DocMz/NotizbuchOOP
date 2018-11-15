f402ccc36e7342973d18f790f2ad007852c0f2ef
DocMz <moritz.zier@web.de>
2018-11-15 11:34:53 +0100

    init

--- NotizbuchOOP/Form1.Designer.cs
+++ NotizbuchOOP/Form1.Designer.cs
@@ -28,26 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lb_Notizen = new System.Windows.Forms.ListBox();
             this.cb_ListenAuswahl = new System.Windows.Forms.ComboBox();
             this.lbl_ComboBox = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
-            // 
-            // lb_Notizen
-            // 
-            this.lb_Notizen.FormattingEnabled = true;
-            this.lb_Notizen.Location = new System.Drawing.Point(12, 78);
-            this.lb_Notizen.Name = "lb_Notizen";
-            this.lb_Notizen.Size = new System.Drawing.Size(214, 212);
-            this.lb_Notizen.TabIndex = 0;
             // 
             // cb_ListenAuswahl
             // 
             this.cb_ListenAuswahl.FormattingEnabled = true;
             this.cb_ListenAuswahl.Location = new System.Drawing.Point(12, 25);
             this.cb_ListenAuswahl.Name = "cb_ListenAuswahl";
-            this.cb_ListenAuswahl.Size = new System.Drawing.Size(214, 21);
+            this.cb_ListenAuswahl.Size = new System.Drawing.Size(222, 21);
             this.cb_ListenAuswahl.TabIndex = 2;
             // 
             // lbl_ComboBox
@@ -59,24 +56,81 @@
             this.lbl_ComboBox.TabIndex = 4;
             this.lbl_ComboBox.Text = "Notizbücher";
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 62);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Notizen";
+            this.button1.Location = new System.Drawing.Point(12, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Notizen";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Einkauf";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(164, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Hausaufg.";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 53);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Buch hinzufügen";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(127, 53);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Buch entfernen";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 127);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(222, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(240, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(252, 264);
+            this.listBox1.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(238, 300);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(504, 297);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lbl_ComboBox);
             this.Controls.Add(this.cb_ListenAuswahl);
-            this.Controls.Add(this.lb_Notizen);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -88,11 +142,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox lb_Notizen;
         private System.Windows.Forms.ComboBox cb_ListenAuswahl;
         private System.Windows.Forms.Label lbl_ComboBox;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
